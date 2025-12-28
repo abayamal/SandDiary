dfeaba95665759c275a4d65ce55a14679020d96e
abayamal <61781282+abayamal@users.noreply.github.com>
2025-12-28 10:51:08 +0530

    create login and sign up template

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,11 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "./Components/Dashboard";
+import GuestLayout from "./Components/GuestLayout";
+import Login from "./Views/Login";
+import Signup from "./Views/Signup";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Dashboard />,
-    }
+        element:<GuestLayout/>,
+        children: [
+            {
+                path: "/login",
+                element: <Login/>
+            },
+            {
+                path: "/signup",
+                element: <Signup/>
+            }
+           
+        ]
+    },
+   
 ])
 
 export default router;
