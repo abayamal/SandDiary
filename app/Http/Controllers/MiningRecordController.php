403365a93beacabd68865920b67780cee1d50a3a
abayamal <61781282+abayamal@users.noreply.github.com>
2026-02-05 22:10:43 +0530

    feat: implement daily mining record storage in backend database

--- app/Http/Controllers/MiningRecordController.php
+++ app/Http/Controllers/MiningRecordController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MiningRecord;
 use Illuminate\Http\Request;
+use App\Models\MiningRecordItem;
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests\StoreMiningRecordRequest;
 
 class MiningRecordController extends Controller
@@ -20,7 +23,39 @@ class MiningRecordController extends Controller
      */
     public function store(StoreMiningRecordRequest $request)
     {
-        //
+        $valdatedData = $request->validated();
+
+        try {
+            // Wrap in a transaction to ensure atomicity
+            $miningRecord = DB::transaction(function () use ($valdatedData){
+
+                // Create main mining record
+                $miningRecord = MiningRecord::create([
+                    'date'=>$valdatedData['date']
+                ]);
+
+                $items = collect($valdatedData['records'])->map(fn($record)=>[
+                        'mining_record_id'=>$miningRecord->id,
+                        'worker_id'=>$record['workerId'],
+                        'volume'=>$record['volume'],
+                        'number_of_loads'=>$record['numberOfLoads'],
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                ])->toArray();
+
+                MiningRecordItem::insert($items);
+
+                return $miningRecord;
+
+            });
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to create record', 'error' => $e->getMessage()], 500);
+        }
+
+        return response()->json([
+            'message'=>'Daily mining record created successfully',
+            'data'=> $miningRecord->load('items')
+        ],200);
     }
 
     /**
---
+++ app/Models/MiningRecord.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\MiningRecordItem;
+use Illuminate\Database\Eloquent\Model;
+
+class MiningRecord extends Model
+{
+
+    protected $fillable = ['date'];
+    
+    public function items(){
+        return $this->hasMany(MiningRecordItem::class);
+    }
+}
